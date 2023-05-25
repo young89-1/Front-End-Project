d9ac20d5e15d9b6d2caf6b8e011bbb17a7fb3f37
mister28 <mattreid321@gmail.com>
2023-05-25 18:28:44 -0500

    Updating script file

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-
+// This is the input for the button
+document.getElementById("btn") 
+    .addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("submit");
+    });
 
 // this is the weather API
 function fetchWeatherData() {
