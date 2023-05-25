fab5db4ab79b67e62791ea26c5e69b41044df18a
young89-1 <mny76ers@yahoo.com>
2023-05-25 19:44:10 -0400

    weather function added

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ document.getElementById("btn")
     e.preventDefault();
     console.log("submit");
     });
+
+// this is the weather API
+function fetchWeatherData() {
+    fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=24S9KU9RJJ6BBP3MT9EMRXLF3')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    }
+    
+    fetchWeatherData();
